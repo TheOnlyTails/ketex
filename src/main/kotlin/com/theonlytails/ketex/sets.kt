d6fbfc99e7081a1a49e1428cb9f35295fbf052e2
theonlytails <theonlytails@theonlytails.com>
2022-04-18 20:29:23 +0300

    Documented for KetexSet#intersect and KetexSet#not. Removed an experimental operator to declare sets.

--- src/main/kotlin/com/theonlytails/ketex/sets.kt
+++ src/main/kotlin/com/theonlytails/ketex/sets.kt
@@ -5,12 +5,22 @@ class KetexSet(private var negate: Boolean) : KetexFragment(), KetexToken {
     @KetexMarker
     override operator fun CharRange.unaryPlus() = add { "$first-$last" }
 
+    /**
+     * Intersects this set with another set (`[\w&&[^\d]]`), matching the result.
+     *
+     * > Matches the intersection between the two character classes.
+     * > Useful if you want to white or blacklist certain characters from a broader class.
+     */
     @KetexMarker
     infix fun intersect(other: KetexSet) = add { "&&$other" }
 
     @KetexMarker
     override fun toString() = "[" + (if (negate) "^" else "") + rawOutput + "]"
 
+    /**
+     * Negates the current set (`[^abcedfu]`), making it match any character *not* in the set.
+     */
+    @KetexMarker
     operator fun not() = apply {
         negate = !negate
     }
@@ -27,6 +37,3 @@ class KetexSet(private var negate: Boolean) : KetexFragment(), KetexToken {
 context(KetexFragment)
 @KetexMarker
 inline fun set(negate: Boolean = false, block: KetexSet.() -> Unit) = KetexSet(negate).apply(block)
-
-@KetexMarker
-inline operator fun KetexFragment.get(block: KetexSet.() -> Unit) = KetexSet(false).apply(block)
